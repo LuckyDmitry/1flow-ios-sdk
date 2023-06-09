6358268b68d6f32180bb902addbe65bbee312f57
LuckyDmitry <48796062+LuckyDmitry@users.noreply.github.com>
2023-06-09 13:42:40 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "OneFlow",
+    name: "_1Flow",
     platforms: [
         .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "OneFlow",
-            targets: ["_1Flow"]),
+            name: "_1Flow",
+            targets: ["OneFlow"]),
     ],
     
     dependencies: [
@@ -22,7 +22,7 @@ let package = Package(
     
     targets: [
         .target(
-            name: "_1Flow",
+            name: "OneFlow",
             path: "Survey",
             exclude: [
                 "1Flow",
