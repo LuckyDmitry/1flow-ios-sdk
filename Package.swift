275e0066fc06a3c2116a3cb297dad9c76ffb3257
LuckyDmitry <48796062+LuckyDmitry@users.noreply.github.com>
2023-06-09 13:46:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "_1Flow",
+    name: "OneFlow",
     platforms: [
         .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "_1Flow",
+            name: "OneFlow",
             targets: ["OneFlow"]),
     ],
     
